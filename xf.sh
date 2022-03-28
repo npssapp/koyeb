a14f682c4dd0dd7db88be74e9d77a7fb8d8c6912
npssapp <99618769+npssapp@users.noreply.github.com>
2022-03-29 00:33:00 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "84537d48-bb73-4fa4-ade5-15a5b68e8be7",
+                        "id": "11111111-1111-1111-1111-111111111111",
                         "alterId": 0
                     }
                 ],
